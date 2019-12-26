3d2644cd37eb939602bf93e66c9ebed1eb8be796
ahmed-muhamed-2 <ahmed.muhmd.93+github@gmail.com>
2019-12-26 17:40:47 +0200

    small test

--- main.js
+++ main.js
@@ -56,4 +56,23 @@ nCallBack.onReceive = incomingMsg => {
   }
 };
 
+// implement other nandbox.Callback() as per your bot need
+nCallBack.onReceiveObj = obj => console.log("received object: ", obj);
+nCallBack.onClose = () => console.log("ONCLOSE");
+nCallBack.onError = () => console.log("ONERROR");
+nCallBack.onChatMenuCallBack = chatMenuCallback => {};
+nCallBack.onInlineMessageCallback = inlineMsgCallback => {};
+nCallBack.onMessagAckCallback = msgAck => {};
+nCallBack.onUserJoinedBot = user => {};
+nCallBack.onChatMember = chatMember => {};
+nCallBack.onChatAdministrators = chatAdministrators => {};
+nCallBack.userStartedBot = user => {};
+nCallBack.onMyProfile = user => {};
+nCallBack.onUserDetails = user => {};
+nCallBack.userStoppedBot = user => {};
+nCallBack.userLeftBot = user => {};
+nCallBack.permanentUrl = permenantUrl => {};
+nCallBack.onChatDetails = chat => {};
+nCallBack.onInlineSearh = inlineSearch => {};
+
 client.connect(TOKEN, nCallBack);
